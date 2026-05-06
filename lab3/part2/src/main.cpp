eae7ef74c6c52ba4dbc803ab6f9ed54603d49504
SpanishMoss00 <spanishmoss00@gmail.com>
2026-05-06 16:30:21 -0700

    scheduler complete

--- lab3/part2/src/main.cpp
+++ lab3/part2/src/main.cpp
@@ -23,8 +23,21 @@ typedef struct TCB {
   unsigned int period; // period of the task in ms
 } TCB;
 
+int schemeTable[3][4] = {
+                  {2, 3, 4, 5},
+                  {5, 4, 2, 3},
+                  {2, 2, 2, 2}
+                  };
+
 TCB taskList[MAX_TASKS];
 TCB *currentTask;
+
+void writeRow(int row, String line) {
+  lcd.setCursor(row,0);
+  lcd.print("                ");
+  lcd.setCursor(row,0);
+  lcd.print(line);
+}
 
 void taskA() {
   // Task A code here
@@ -48,6 +61,11 @@ void taskD() {
 void taskE() {
   // Task E code here
   static unsigned int currentScheme = 1; // wrap around from 1 to 3
+  for (int i = 0; i < MAX_TASKS - 1; i++) {
+    taskList[i].priority = schemeTable[currentScheme - 1][i];
+  }
+  currentScheme = (currentScheme % 3) + 1;
+  sleep_me(PRIORITY_UPDATE_INTERVAL);
 }
 
 void sleep_me(unsigned int ms) {
@@ -61,24 +79,49 @@ void halt_me() {
 }
 
 void setup() {
+  lcd.begin();
   taskList[0] = {taskA, READY, 0, 2, LED_BLINK_INTERVAL};           // TASK A: Blinking LED 
   taskList[1] = {taskB, READY, 0, 3, LCD_UPDATE_INTERVAL};          // TASK B: LCD Counter
   taskList[2] = {taskC, READY, 0, 4, MUSIC_PLAY_INTERVAL};          // TASK C: Music Player
   taskList[3] = {taskD, READY, 0, 5, ALPHABET_PRINT_INTERVAL};      // TASK D: Alphabet Printer
-  taskList[4] = {taskE, SLEEPING, 0, 1, PRIORITY_UPDATE_INTERVAL};  // TASK E: Priority Updater
+  taskList[4] = {taskE, READY, 0, 1, PRIORITY_UPDATE_INTERVAL};  // TASK E: Priority Updater
 
 }
 
 void loop() {
-  bool taskNotFound = true;
-  while (taskNotFound) {
-   int currentPriority = 1;
-   for (int i = 0; i < MAX_TASKS; i++) {
-      if (taskList[i].priority == currentPriority && taskList[i].state != SLEEPING) {
+  bool ranTask = false; // whether or not we have actually run a task yet
 
+  // loop through every priority
+  for (int currentPriority = 1; currentPriority <= 5; currentPriority++) {
+
+    // loop through every task in the list
+    for (int i = 0; i < MAX_TASKS; i++) { 
+      currentTask = &taskList[i];
+
+      // if the task matches the current priority level
+      if (taskList[i].priority == currentPriority) {
+
+        // Behavior based on the task's state
+        if (taskList[i].state == HALTED) { // skip if halted
+          continue;
+        // If sleeping, check if it should wake up, and continue search for more tasks to run
+        } else if (taskList[i].state == SLEEPING) {
+          if (millis() - taskList[i].lastRunTime >= taskList[i].period) {
+            taskList[i].state = READY;
+          }
+          continue;
+        // run the task if it is ready, then break out (search complete)
+        } else if (taskList[i].state == READY) {
+          taskList[i].state = RUNNING;
+          taskList[i].function(); 
+          // return from task here, tasks are responsible for calling sleep_me and halt_me
+          ranTask = true;
+          break;
+        } else { // skip if running or unknown state
+          continue;
+        }
       }
     }
-    currentPriority++;
-    if (currentPriority > 5) taskNotFound = false; // exit if there were no tasks to run
+    if (ranTask) break; // restart search from highest priority level if we ran a task
   }
 }
