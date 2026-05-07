b3d43e0dd18f1b9deecfa429bc63f691e768e0bb
SpanishMoss00 <spanishmoss00@gmail.com>
2026-05-07 13:20:03 -0700

    interuppts

--- lab3/part3/src/main.cpp
+++ lab3/part3/src/main.cpp
@@ -1,6 +1,8 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <LiquidCrystal_I2C.h>
+#include <Arduino.h>
 
 
 //===============> TODO:
@@ -12,7 +14,19 @@
 
 // Pins
 #define BUTTON_PIN 1 //make sure to input pullup
+#define LCD_ADDRESS 0x27
 
+LiquidCrystal_I2C lcd(LCD_ADDRESS, 16, 2);
+
+volatile unsigned long counter = 0;
+
+// Write a line of text to one of the LCD rows
+void writeRow(int row, String line) {
+  lcd.setCursor(0,row);
+  lcd.print("                ");
+  lcd.setCursor(0,row);
+  lcd.print(line);
+}
 
 class MyCallbacks: public BLECharacteristicCallbacks {
    void onWrite(BLECharacteristic *pCharacteristic) {
@@ -24,10 +38,14 @@ class MyCallbacks: public BLECharacteristicCallbacks {
 
 
 // ==============> TODO: Write your timer ISR here.
-
+void timerISR() {
+  counter++; 
+}
 
 // ==============> TODO: Create an ISR function to handle button press here.
-
+void buttonISR() {
+  but_pressed = true;
+}
 
 void setup() {
  BLEDevice::init("MyESP32");
@@ -48,10 +66,19 @@ void setup() {
 
    // =========> TODO: Initialize LCD display
    //
+   lcd.begin();
+
    // =========> TODO: create a timer, attach an interrupt, set an alarm which will
    //                  update the counter every second.
    //
-    // ========> TODO: Set button pin as input and attach an interrupt
+   hw_timer_t *interruptTimer = timerBegin(0, 80, true);
+   timerAttachInterrupt(interruptTimer, timerISR, true);
+   timerAlarmWrite(interruptTimer, 1000000, true);
+   timerAlarmEnable(interruptTimer);
+
+   // ========> TODO: Set button pin as input and attach an interrupt
+   pinMode(BUTTON_PIN, INPUT_PULLUP);
+   attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonISR, FALLING); 
 }
 
 
