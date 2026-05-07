33f8514df1c8aaa1d83526b8d553229984563049
Boonanak <sean.bubernak@gmail.com>
2026-05-07 12:24:43 -0700

    Skeleton code for BLE usage

--- lab3/part3/src/main.cpp
+++ lab3/part3/src/main.cpp
@@ -1,18 +1,63 @@
-#include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+//===============> TODO:
+// Generate random Service and Characteristic UUIDs: https://www.uuidgenerator.net/
+
+
+#define SERVICE_UUID        ""
+#define CHARACTERISTIC_UUID ""
+
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+   void onWrite(BLECharacteristic *pCharacteristic) {
+     // =========> TODO: This callback function will be invoked when signal is
+     // 		     received over BLE. Implement the necessary functionality that
+     //		     will trigger the message to the LCD.
+   }
+};
+
+
+// ==============> TODO: Write your timer ISR here.
+
+
+// ==============> TODO: Create an ISR function to handle button press here.
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ BLEDevice::init("MyESP32");
+ BLEServer *pServer = BLEDevice::createServer();
+ BLEService *pService = pServer->createService(SERVICE_UUID);
+ BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                        CHARACTERISTIC_UUID,
+                                        BLECharacteristic::PROPERTY_READ |
+                                        BLECharacteristic::PROPERTY_WRITE
+                                      );
+
+
+ pCharacteristic->setCallbacks(new MyCallbacks());
+ pService->start();
+ BLEAdvertising *pAdvertising = pServer->getAdvertising();
+ pAdvertising->start();
+
+
+   // =========> TODO: Initialize LCD display
+   //
+   // =========> TODO: create a timer, attach an interrupt, set an alarm which will
+   //                  update the counter every second.
+   //
+    // ========> TODO: Set button pin as input and attach an interrupt
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+ // =========> TODO: Print out an incrementing counter to the LCD.
+ //                  If a signal has been received over BLE, print out “New
+ //                  Message!” on the LCD.
+ //                  If the button has been pressed, print out "Button Pressed"
+ //                  on the LCD.
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
 }
