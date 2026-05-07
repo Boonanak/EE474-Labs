9310a79efdc17b6afb2b26fa5d2e3c33e3c60a83
Boonanak <sean.bubernak@gmail.com>
2026-05-07 13:19:12 -0700

    Wrote parts of part 3

--- lab3/part3/src/main.cpp
+++ lab3/part3/src/main.cpp
@@ -2,23 +2,23 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 
-
-//===============> TODO:
 // Generate random Service and Characteristic UUIDs: https://www.uuidgenerator.net/
 
-
-#define SERVICE_UUID        ""
-#define CHARACTERISTIC_UUID ""
+#define SERVICE_UUID        "bfcad23b-c6d5-4dc4-bf77-bb07f3f76e14"
+#define CHARACTERISTIC_UUID "624dde7b-a518-45c1-bb2f-ba69ccf1e9ff"
 
 // Pins
 #define BUTTON_PIN 1 //make sure to input pullup
 
+volatile bool BLE_tripped = false;
+volatile bool but_pressed = false;
 
 class MyCallbacks: public BLECharacteristicCallbacks {
    void onWrite(BLECharacteristic *pCharacteristic) {
      // =========> TODO: This callback function will be invoked when signal is
      // 		     received over BLE. Implement the necessary functionality that
      //		     will trigger the message to the LCD.
+    BLE_tripped = true;    
    }
 };
 
@@ -62,5 +62,17 @@ void loop() {
  //                  If the button has been pressed, print out "Button Pressed"
  //                  on the LCD.
 
+  writeRow(0, counter);
 
+  if (but_pressed) {
+    but_pressed = false;
+    writeRow(1, "Button Pressed");
+    delay(2000);
+  }
+
+  if (BLE_tripped) {
+    BLE_tripped = false;
+    writeRow(1, "New Message!");
+    delay(2000);
+  }
 }
