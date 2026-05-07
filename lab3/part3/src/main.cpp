55028d97b8cc8f4c528dad31e00a0e1966eaf9ea
Boonanak <sean.bubernak@gmail.com>
2026-05-07 12:45:34 -0700

    copied sean over

--- lab3/part3/src/main.cpp
+++ lab3/part3/src/main.cpp
@@ -1,4 +1,32 @@
-#include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+
+//===============> TODO:
+// Generate random Service and Characteristic UUIDs: https://www.uuidgenerator.net/
+
+
+#define SERVICE_UUID        ""
+#define CHARACTERISTIC_UUID ""
+
+// Pins
+#define BUTTON_PIN 1 //make sure to input pullup
+
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+   void onWrite(BLECharacteristic *pCharacteristic) {
+     // =========> TODO: This callback function will be invoked when signal is
+     // 		     received over BLE. Implement the necessary functionality that
+     //		     will trigger the message to the LCD.
+   }
+};
+
+
+// ==============> TODO: Write your timer ISR here.
+
+
+// ==============> TODO: Create an ISR function to handle button press here.
 
 // put function declarations here:
 int myFunction(int, int);
