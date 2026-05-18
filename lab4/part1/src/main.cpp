78cb1c25efecd43327462141f9542dadbfa93ced
Boonanak <sean.bubernak@gmail.com>
2026-05-18 13:30:17 -0700

    Added lib and skeleton code to part 2

--- lab4/part1/src/main.cpp
+++ lab4/part1/src/main.cpp
@@ -1,18 +1,45 @@
-#include <Arduino.h>
+setup()
+// ====================> TODO:
+//         1. Initialize pins, serial, LCD, etc
+//         2. Create binary semaphore for synchronization of light level data.
+//         3. Create Tasks
+//          - Create the `Light Detector Task` and assign it to Core 0.
+//          - Create `LCD Task` and assign it to Core 0.
+//          - Create `Anomaly Alarm Task` and assign it to Core 1.
+//          - Create `Prime Calculation Task` and assign it to Core 1.
+loop()
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+Light Detector Task (Core 0)
+// ====================> TODO:
+//          1. Initialize Variables
+//          2. Loop Continuously
+//           - Read light level from the photoresistor.
+//           - Take semaphore
+//           - Calculate the simple moving average and update variables.
+//           - Give semaphore to signal data is ready.
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+LCD Task (Core 0)
+// ====================> TODO:
+//          1. Initialize Variables
+//           2. Loop Continuously
+//            - Wait for semaphore.
+//            - If data has changed, update the LCD with the new light level and SMA.
+//            - Give back the semaphore.
+
+
+Anomaly Alarm Task (Core 1)
+// ====================> TODO:
+//            1. Loop Continuously
+//             - Wait for semaphore.
+//             - Check if SMA indicates a light anomaly (outside thresholds).
+//             - If an anomaly is detected, flash a LED signal.
+//             - Give back the semaphore.
+
+
+Prime Calculation Task (Core 1)
+// ====================> TODO:
+//            1. Loop from 2 to 5000
+//             - Check if the current number is prime.
+//             - If prime, print the number to the serial monitor.
